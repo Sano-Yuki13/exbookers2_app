29f988975980a4adc9cd21f06875195614149d06
SanoYuki <134158109+Sano-Yuki13@users.noreply.github.com>
2023-08-03 19:13:23 +0000

    ex4 fix

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -12,7 +12,7 @@ class RelationshipsController < ApplicationController
 
   def followings
     user = User.find(params[:user_id])
-    @users = user.followings
+   @users = user.followings
   end
 
   def followers
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_one_attached :profile_image
 
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
@@ -15,6 +14,7 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
+  has_one_attached :profile_image
   validates :name,  uniqueness: true, length: {in: 2..20 }
   validates :introduction, length: {maximum: 50}
 
@@ -23,11 +23,11 @@ class User < ApplicationRecord
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
 
-  def follow(user_id)
+  def follow(user)
     relationships.create(followed_id: user_id)
   end
 
-  def unfollow(user_id)
+  def unfollow(user)
     relationships.find_by(followed_id: user_id).destroy
   end
 
